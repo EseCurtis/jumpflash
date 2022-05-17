3b03c7894b9c20e44138e3a79c6dc81ee6def64f
esecurtis <curtisese52@gmail.com>
2022-05-17 20:19:10 +0100

    first commic to jumpflash

---
+++ ws.js
@@ -0,0 +1,32 @@
+// Importing the required modules
+const WebSocketServer = require('ws');
+const fetch = require('node-fetch');
+ 
+// Creating a new websocket server
+const wss = new WebSocketServer.Server({ port: 8080 })
+ 
+// Creating connection using websocket
+wss.on("connection", ws => {
+    console.log("new client connected");
+    // sending message
+    ws.on("message", data => {
+        let url = data
+        let prevResp = ''
+        setInterval(() => {
+            fetch(url)
+            .then(res => res.text())
+            .then(text => {
+                if(prevResp !== text) ws.send((text)), prevResp = text
+            })
+        }, 1000);
+    });
+    // handling what to do when clients disconnects from server
+    ws.on("close", () => {
+        console.log("the client has disconnected");
+    });
+    // handling client connection error
+    ws.onerror = function () {
+        console.log("Some Error occurred")
+    }
+});
+console.log("The WebSocket server is running on port 8080");
